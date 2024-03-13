9dcd6c28dec9d319dfc2bd245d4dda24a695b0f3
kimilgon <moses011010@gmail.com>
2024-03-14 01:00:27 +0900

    feat: package json과 config파일들 업데이트

--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const prod = process.env.NODE_ENV === "production";
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  disable: prod ? false : true,
+});
 
-module.exports = nextConfig
+const nextConfig = withPWA({
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tailwindui.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatar.maplestory.nexon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "open.api.nexon.com",
+      },
+    ],
+    dangerouslyAllowSVG: true,
+  },
+});
+
+module.exports = nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,38 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      keyframes: {
+        blink: {
+          "0%, 100%": {
+            opacity: 1,
+          },
+          "50%": { opacity: 0.5 },
+        },
+      },
+      animation: {
+        "blink-text": "blink 1s linear infinite",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
