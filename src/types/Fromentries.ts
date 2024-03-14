3a0390adb4f0a03f229ba79bc06e968d04738117
kimilgon <moses011010@gmail.com>
2024-03-14 18:54:18 +0900

    type : Fromentries와 entries 타입 몽키패칭

--- src/types/Fromentries.ts
+++ src/types/Fromentries.ts
@@ -1,5 +1,17 @@
+type ArrayElement<A> = A extends readonly (infer T)[] ? T : never;
+type DeepWriteable<T> = { -readonly [P in keyof T]: DeepWriteable<T[P]> };
 type Cast<X, Y> = X extends Y ? X : Y;
-
-type FromEntriesV1<T> = T extends [infer Key, any][]
-  ? { [K in Cast<Key, string>]: any }
+type FromEntries<T> = T extends [infer Key, any][]
+  ? { [K in Cast<Key, string>]: Extract<ArrayElement<T>, [K, any]>[1] }
   : { [key in string]: any };
+
+export type FromEntriesWithReadOnly<T> = FromEntries<DeepWriteable<T>>;
+
+declare global {
+  interface ObjectConstructor {
+    fromEntries<T>(obj: T): FromEntriesWithReadOnly<T>;
+    entries<T>(obj: T): {
+      [K in keyof T]: [K, T[K]];
+    }[keyof T][];
+  }
+}
